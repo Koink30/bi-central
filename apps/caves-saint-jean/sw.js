36ed0bccdc6c0546e313337397e28cd4c4302055
Koink30 <135063024+Koink30@users.noreply.github.com>
2026-09-15 06:41:45 +0200

    Allow negative stock and multi-product sales

--- apps/caves-saint-jean/sw.js
+++ apps/caves-saint-jean/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "caves-saint-jean-v2";
+const CACHE = "caves-saint-jean-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./offline.html", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", event => {
